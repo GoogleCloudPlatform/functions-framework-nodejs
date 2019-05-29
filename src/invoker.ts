9d0d720fa89313b1ab0fbd09a30a0a21f04d4a8b
Adam Ross <adamross@google.com>
2019-05-29 15:55:52 -0700

    Log errors after "finished functions"

--- src/invoker.ts
+++ src/invoker.ts
@@ -352,7 +352,7 @@ function makeHttpHandler(execute: HttpFunction): express.RequestHandler {
     // Catch unhandled errors originating from this request.
     d.on('error', err => {
       if (res.locals.functionExecutionFinished) {
-        console.log('Ignoring exception from a finished function');
+        console.error(err);
       } else {
         res.locals.functionExecutionFinished = true;
         logAndSendError(err, res);
