c5d2b20daae968208e045af0963324092b743271
Huy Pham <11185878+hdp617@users.noreply.github.com>
2020-06-04 16:32:02 -0700

    fix: Remove unused test functions

--- test/data/without_main/function.js
+++ test/data/without_main/function.js
@@ -4,40 +4,10 @@
  * @param {!Object} req request context.
  * @param {!Object} res response context.
  */
-function testHttpFunction (res, req) {
+function testFunction (req, res) {
   return 'PASS'
 };
 
-/**
- * Test event function to test function loading.
- *
- * @param {!Object} data event payload.
- * @param {!Object} context event metadata.
- */
-function testEventFunction (data, context) {
-  console.log(data);
-  console.log(context);
-  return 'PASS';
-};
-
-/**
- * Test cloudevent function to test function loading.
- *
- * @param {!Object} cloudevent CloudEvents context.
- */
-function testCloudEventFunction (cloudevent) {
-  console.log(`specversion: ${cloudevent.specversion || 'undefined'}`);
-  console.log(`type: ${cloudevent.type || 'undefined'}`);
-  console.log(`source: ${cloudevent.source || 'undefined'}`);
-  console.log(`subject: ${cloudevent.subject || 'undefined'}`);
-  console.log(`id: ${cloudevent.id || 'undefined'}`);
-  console.log(`time: ${cloudevent.time || 'undefined'}`);
-  console.log(`datacontenttype: ${cloudevent.datacontenttype || 'undefined'}`);
-  return 'PASS';
-};
-
 module.exports = {
-  testHttpFunction,
-  testEventFunction,
-  testCloudEventFunction,
+  testFunction,
 }
