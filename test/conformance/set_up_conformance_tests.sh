44499953647cdbd17182d9a1f83020d5189e828c
Huy Pham <11185878+hdp617@users.noreply.github.com>
2020-11-20 17:10:13 -0800

    fix: use the commit id instead of GITHUB_SHA

--- test/conformance/set_up_conformance_tests.sh
+++ test/conformance/set_up_conformance_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pushd test/conformance
-sed -i "s/functions-framework-nodejs/functions-framework-nodejs#$GITHUB_SHA/" package.json
+sed -i "s/functions-framework-nodejs/functions-framework-nodejs#${git rev-parse --short HEAD}/" package.json
 popd
