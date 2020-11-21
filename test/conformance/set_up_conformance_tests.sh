6fe354df764164863db695d72da816e4ca84082c
Huy Pham <11185878+hdp617@users.noreply.github.com>
2020-11-20 18:00:18 -0800

    fix: add case for pull requests

--- test/conformance/set_up_conformance_tests.sh
+++ test/conformance/set_up_conformance_tests.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-commit_sha="$(git rev-parse HEAD)"
+commit_sha="${GITHUB_SHA}"
+# GITHUB_SHA is not the commit SHA for pull requests.
+if [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
+    commit_sha=$(cat $GITHUB_EVENT_PATH | jq -r .pull_request.head.sha)
+fi
 sed -i "s/functions-framework-nodejs/functions-framework-nodejs#$commit_sha/" package.json
