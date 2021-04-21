f67d11d9305965e6df2a10389e64389fa623f689
Grant Timmerman <744973+grant@users.noreply.github.com>
2021-04-21 15:40:03 -0500

    feat: updates event and cloudevent interfaces (#276)
    - Updates the function interfaces for the Node FF
      - Adds a `LegacyEvent` interface
      - Updates `CloudEvent` interface to CE 1.0 (must have been <1.0 with `schemaurl`)

--- src/cloudevents.ts
+++ src/cloudevents.ts
@@ -47,7 +47,9 @@ export function getBinaryCloudEventContext(
   const context: CloudEventsContext = {};
   for (const name in req.headers) {
     if (name.startsWith('ce-')) {
-      const attributeName = name.substr('ce-'.length);
+      const attributeName = name.substr(
+        'ce-'.length
+      ) as keyof CloudEventsContext;
       context[attributeName] = req.header(name);
     }
   }
