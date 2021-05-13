7bf9b88549a552ad9068bd878139a7f934c6c750
Matthew Robertson <mattrobertson@google.com>
2021-05-13 17:44:01 +0000

    tidy up comments

--- src/middelware/legacy_event_to_cloudevent.ts
+++ src/middelware/legacy_event_to_cloudevent.ts
@@ -95,6 +95,9 @@ const getLegacyEvent = (request: Request): LegacyEvent => {
   return {context, data};
 };
 
+/**
+ * The CloudEvent service, resource and subject fields parsed from a GCF event context.
+ */
 interface ParsedResource {
   service: string;
   resource: string;
@@ -103,6 +106,8 @@ interface ParsedResource {
 
 /**
  * Splits a background event's resource into a CloudEvent service, resource, and subject.
+ * @param context the GCF event context to parse.
+ * @returns the CloudEvent service, resource and subject fields for the given GCF event context.
  */
 export const splitResource = (
   context: CloudFunctionsContext
@@ -152,8 +157,8 @@ export const splitResource = (
 };
 
 /**
- * Express middleware to convert legacy GCF requests to cloud events. This enables functions
- * using the "CLOUD_EVENT" signature type to accept requests from a legacy event producer.
+ * Express middleware to convert legacy GCF requests to CloudEvents. This enables functions
+ * using the "cloudevent" signature type to accept requests from a legacy event producer.
  * @param req express request object
  * @param res express response object
  * @param next function used to pass control to the next middleware function in the stack
