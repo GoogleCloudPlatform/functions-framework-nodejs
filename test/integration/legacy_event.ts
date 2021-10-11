6e9f1038a1738c784ec59701263070b659f2cc07
Grant Timmerman <timmerman+devrel@google.com>
2021-10-11 16:06:58 -0700

    ci: add traceparent to another test
    Signed-off-by: Grant Timmerman <timmerman+devrel@google.com>

--- test/integration/legacy_event.ts
+++ test/integration/legacy_event.ts
@@ -136,6 +136,7 @@ describe('Event Function', () => {
         specversion: '1.0',
         subject: 'test-subject',
         time: '2020-05-13T01:23:45Z',
+        traceparent: '00-65088630f09e0a5359677a7429456db7-97f23477fb2bf5ec-01',
         type: 'com.google.cloud.storage',
       },
     },
