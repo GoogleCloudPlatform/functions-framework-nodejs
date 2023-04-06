c5015ce1555b6c1a26ca9c39c48c6b87fbdacd97
Gareth George <garethgeorge@google.com>
2023-04-06 00:56:02 +0000

    feat: implement typed function signature

--- src/functions.ts
+++ src/functions.ts
@@ -74,16 +74,27 @@ export interface CloudEventFunction<T = unknown> {
 export interface CloudEventFunctionWithCallback<T = unknown> {
   (cloudEvent: CloudEvent<T>, callback: Function): any;
 }
+
+/**
+ * A Typed function handler that may return a value or a promise.
+ * @public
+ */
+export interface TypedFunction<T = unknown, U = unknown> {
+  handler: (req: T) => U | Promise<U>;
+  format: InvocationFormat<T, U>;
+}
+
 /**
  * A function handler.
  * @public
  */
-export type HandlerFunction<T = unknown> =
+export type HandlerFunction<T = unknown, U = unknown> =
   | HttpFunction
   | EventFunction
   | EventFunctionWithCallback
   | CloudEventFunction<T>
-  | CloudEventFunctionWithCallback<T>;
+  | CloudEventFunctionWithCallback<T>
+  | TypedFunction<T, U>;
 
 /**
  * A legacy event.
@@ -137,3 +148,78 @@ export interface CloudFunctionsContext {
  * @public
  */
 export type Context = CloudFunctionsContext | CloudEvent<unknown>;
+
+/**
+ * InvocationRequest represents the properties of an invocation over HTTP.
+ * @public
+ */
+export interface InvocationRequest {
+  /** Returns the request body as either a string or a Buffer if the body is binary. */
+  body(): string | Buffer;
+  /** Header returns the value of the specified header */
+  header(header: string): string | undefined;
+}
+
+/**
+ * InvocationResponse interface describes the properties that can be set on
+ * an invocation response.
+ * @public
+ */
+export interface InvocationResponse {
+  /** Sets a header on the response. */
+  setHeader(key: string, value: string): void;
+  /** Writes a chunk of data to the response. */
+  write(data: string | Buffer): void;
+  /** Ends the response, must be called once at the end of writing. */
+  end(data: string | Buffer): void;
+}
+
+/**
+ * The contract for a request deserializer and response serializer.
+ * @public
+ */
+export interface InvocationFormat<T, U> {
+  /**
+   * Creates an instance of the request type from an invocation request.
+   *
+   * @param request the request body as raw bytes
+   */
+  deserializeRequest(request: InvocationRequest): T | Promise<T>;
+
+  /**
+   * Writes the response type to the invocation result.
+   *
+   * @param responseWriter interface for writing to the invocation result
+   * @param response the response object
+   */
+  serializeResponse(
+    responseWriter: InvocationResponse,
+    response: U
+  ): void | Promise<void>;
+}
+
+/**
+ * Default invocation format for JSON requests.
+ * @public
+ */
+export class JsonInvocationFormat<T, U> implements InvocationFormat<T, U> {
+  deserializeRequest(request: InvocationRequest): T {
+    const body = request.body();
+    if (typeof body !== 'string') {
+      throw new Error('Unsupported Content-Type, expected application/json');
+    }
+    try {
+      return JSON.parse(body);
+    } catch (e) {
+      throw new Error(
+        'Failed to parse malformatted JSON in request: ' +
+          (e as SyntaxError).message
+      );
+    }
+  }
+
+  serializeResponse(responseWriter: InvocationResponse, response: U): void {
+    responseWriter.setHeader('content-type', 'application/json');
+    responseWriter.end(JSON.stringify(response));
+  }
+}
--- src/types.ts
+++ src/types.ts
@@ -19,7 +19,7 @@ export const FUNCTION_STATUS_HEADER_FIELD = 'X-Google-Status';
 /**
  * List of function signature types that are supported by the framework.
  */
-export const SignatureType = ['http', 'event', 'cloudevent'] as const;
+export const SignatureType = ['http', 'event', 'cloudevent', 'typed'] as const;
 
 /**
  * Union type of all valid function SignatureType values.
