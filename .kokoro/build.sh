beb7aa1235348e22909f20f0777e83bfe9f4ce58
Daniel Lee <danielylee@google.com>
2026-06-16 15:57:50 -0700

    chore(release): fix release script (#767)

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -7,35 +7,29 @@ cd "$KOKORO_ARTIFACTS_DIR/git/serverless/functions-framework-nodejs"
 ### Configure Airlock
 # APT
 rm -f /etc/apt/sources.list.d/* /etc/apt/sources.list
-echo 'deb https://us-apt.pkg.dev/remote/artifact-foundry-prod/debian-3p-remote-bookworm bookworm main' | \
-    tee -a  /etc/apt/sources.list.d/artifact-registry.list
+echo 'deb https://us-apt.pkg.dev/remote/artifact-foundry-prod/debian-3p-remote-bookworm bookworm main' |
+  tee -a /etc/apt/sources.list.d/artifact-registry.list
 
 # NPM
-cat > .npmrc <<EOF
+cat >.npmrc <<EOF
 registry=https://us-npm.pkg.dev/artifact-foundry-prod/npm-3p-trusted/
 //us-npm.pkg.dev/artifact-foundry-prod/npm-3p-trusted/:always-auth=true
 EOF
 cp .npmrc "$HOME/.npmrc"
 
-### Build all package.json
+### Build root package
 ARTIFACTS="${KOKORO_ARTIFACTS_DIR}/artifacts"
-mkdir "${ARTIFACTS}"
+mkdir -p "${ARTIFACTS}"
 
-readarray -t package_jsons < <(find "." -type d -name "node_modules" -prune -o -name "package.json" -print)
-for package_file in "${package_jsons[@]}"; do
-  echo "Building package ${package_file}"
-  package_dir="$(dirname "${package_file}")"
-  pushd "${package_dir}"
-  npm ci
-  # npm pack and npm publish needs to be distinct steps to ensure the
-  # artifacts will be stored locally for attestation generation.
-  npm pack --pack-destination="${ARTIFACTS}"
-  popd
-done
+echo "Building root package"
+npm ci
+# npm pack and npm publish needs to be distinct steps to ensure the
+# artifacts will be stored locally for attestation generation.
+npm pack --pack-destination="${ARTIFACTS}"
 
 ### Authenticate to OSS Exit Gate
 # Replace default registry with OSS Exit Gate
-cat > .npmrc <<EOF
+cat >.npmrc <<EOF
 registry=https://us-npm.pkg.dev/oss-exit-gate-prod/ff-releases--npm/
 //us-npm.pkg.dev/oss-exit-gate-prod/ff-releases--npm/:always-auth=true
 EOF
